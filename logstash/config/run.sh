33fa95da113bc80c2d835be05950de8964c2e763
Ubuntu <ubuntu@ip-10-252-157-237.us-west-1.compute.internal>
2017-02-02 11:45:28 +0000

    logstash need to apply until elastic work

--- logstash/config/run.sh
+++ logstash/config/run.sh
@@ -12,6 +12,11 @@
 #
 
 set -e
+host="elasticsearch"
+until curl -XGET $host:9200; do
+  >&2 echo "Elastic is unavailable - sleeping"
+  sleep 1
+done
 
 # Add logstash as command if needed
 if [ "${1:0:1}" = '-' ]; then
