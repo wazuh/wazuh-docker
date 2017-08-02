84005d81459d5f8655efb9fcb2334296d815e5be
david <david@davidkarlsen.com>
2017-08-02 23:26:38 +0200

    Upgrade to ELK 5.5.1

--- kibana/config/wait-for-it.sh
+++ kibana/config/wait-for-it.sh
@@ -5,7 +5,7 @@ set -e
 host="$1"
 shift
 cmd="kibana"
-WAZUH_KIBANA_PLUGIN_URL=${WAZUH_KIBANA_PLUGIN_URL:-https://packages.wazuh.com/wazuhapp/wazuhapp-2.0.1_5.5.0.zip}
+WAZUH_KIBANA_PLUGIN_URL=${WAZUH_KIBANA_PLUGIN_URL:-https://packages.wazuh.com/wazuhapp/wazuhapp-2.0.1_5.5.1.zip}
 
 until curl -XGET $host:9200; do
   >&2 echo "Elastic is unavailable - sleeping"
