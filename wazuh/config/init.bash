000e677839a8323985ef9458916fd1410a58b88b
davinerd <anathema@anche.no>
2017-10-22 13:41:07 +0200

    automatic password generation for wazuh API

--- wazuh/config/init.bash
+++ wazuh/config/init.bash
@@ -5,6 +5,12 @@
 #
 source /data_dirs.env
 
+API_USER="wazadmin"
+API_PASSWD=`openssl rand -base64 32 | head -c 16`
+echo "API credentials are ${API_USER} - ${API_PASSWD}"
+
+/bin/node /var/ossec/api/configuration/auth/htpasswd -c /var/ossec/api/configuration/auth/user -b ${API_USER} ${API_PASSWD}
+
 cd /var/ossec
 for ossecdir in "${DATA_DIRS[@]}"; do
   mv ${ossecdir} ${ossecdir}-template
