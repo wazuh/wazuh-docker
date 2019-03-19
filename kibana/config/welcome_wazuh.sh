c1a33b7185523bc8669234e8ff89c20f53be682f
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2019-03-19 14:35:44 +0100

    kibana welcome

---
+++ kibana/config/welcome_wazuh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $CHANGE_WELLCOME == "true" ]]
+then
+
+    kibana_path="/usr/share/kibana"
+    # Set Wazuh app as the default landing page
+    echo "Set Wazuh app as the default landing page"
+    echo "server.defaultRoute: /app/wazuh" >> /etc/kibana/kibana.yml
+
+    # Redirect Kibana welcome screen to Discover
+    echo "Redirect Kibana welcome screen to Discover"
+    sed -i "s:'/app/kibana#/home':'/app/kibana#/discover':g" $kibana_path/src/ui/public/chrome/directives/global_nav/global_nav.html
+    sed -i "s:'/app/kibana#/home':'/app/kibana#/discover':g" $kibana_path/src/ui/public/chrome/directives/header_global_nav/header_global_nav.js
+fi
