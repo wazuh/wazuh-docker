b7537453e31af6c8a5a911bb1c246f4a08faeda0
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2019-03-20 15:47:14 +0100

    Hide undesired links

--- kibana/config/welcome_wazuh.sh
+++ kibana/config/welcome_wazuh.sh
@@ -3,6 +3,8 @@
 if [[ $CHANGE_WELCOME == "true" ]]
 then
 
+    rm -rf ./optimize/bundles
+
     kibana_path="/usr/share/kibana"
     # Set Wazuh app as the default landing page
     echo "Set Wazuh app as the default landing page"
@@ -12,4 +14,10 @@ then
     echo "Redirect Kibana welcome screen to Discover"
     sed -i "s:'/app/kibana#/home':'/app/wazuh':g" $kibana_path/src/ui/public/chrome/directives/global_nav/global_nav.html
     sed -i "s:'/app/kibana#/home':'/app/wazuh':g" $kibana_path/src/ui/public/chrome/directives/header_global_nav/header_global_nav.js
+
+    # Redirect Kibana welcome screen to Discover
+    echo "Hide undesired links"
+    sed -i 's#visible: true#visible: false#g' $kibana_path/x-pack/plugins/rollup/public/crud_app/index.js
+    sed -i 's#visible: true#visible: false#g' $kibana_path/x-pack/plugins/license_management/public/management_section.js
 fi
+
