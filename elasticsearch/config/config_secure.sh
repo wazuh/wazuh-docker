1ac653037e863857c34ed1a9e28b2c1405a606d0
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2019-05-30 15:23:33 +0200

    TLS for Elasticsearch

--- elasticsearch/config/config_secure.sh
+++ elasticsearch/config/config_secure.sh
@@ -11,9 +11,24 @@ elastic_config_file="/usr/share/elasticsearch/config/elasticsearch.yml"
 
 if [[ $SETUP_PASSWORDS == "yes" ]]; then
 
+echo "Creating certificate and set elasticsearch configuration options"
+
+pushd /usr/share/elasticsearch/config/
+
+unzip elastic-CA.zip
+/usr/share/elasticsearch/bin/elasticsearch-certutil cert --pem --out certs.zip --ca-cert server.CA-signed.crt --ca-key server.CA.key  --ca-pass $CA_PASS
+unzip certs.zip
+
+popd
+
   echo "
-# Required to set the passwords
+# Required to set the passwords and TLS options
 xpack.security.enabled: true
+xpack.security.transport.ssl.enabled: true
+xpack.security.transport.ssl.verification_mode: certificate
+xpack.security.transport.ssl.key: /usr/share/elasticsearch/config/instance/instance.key
+xpack.security.transport.ssl.certificate: /usr/share/elasticsearch/config/instance/instance.crt
+xpack.security.transport.ssl.certificate_authorities: [ \"/usr/share/elasticsearch/config/server.CA-signed.crt\" ]
 " >> $elastic_config_file
 
 fi
