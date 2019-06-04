fa56af712bf374786427bcda6414547d53f3e1cc
Mayte Ariza <mayte.ariza@wazuh.com>
2019-06-04 11:47:07 +0200

    Symbolic links deleted

--- wazuh/config/init.bash
+++ wazuh/config/init.bash
@@ -4,10 +4,17 @@
 #
 # Initialize the custom data directory layout
 #
-source /data_dirs.env
+source /data_files.env
 
+MIRRORING_PATH=/var/ossec/docker-backups
+mkdir ${MIRRORING_PATH}
 cd /var/ossec
-for ossecdir in "${DATA_DIRS[@]}"; do
-  mv ${ossecdir} ${ossecdir}-template
-  ln -s $(realpath --relative-to=$(dirname ${ossecdir}) data)/${ossecdir} ${ossecdir}
+
+for ossecfile in "${DATA_FILES[@]}"; do
+  if [ ! -e ${MIRRORING_PATH}/${ossecfile}  ]
+  then
+    DIR=$(dirname "${ossecfile}")
+    mkdir -p ${MIRRORING_PATH}/${DIR}
+  fi
+  mv ${ossecfile} docker-backups/${ossecfile}
 done
