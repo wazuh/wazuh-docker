b41f833c595fe1f0ad85f1b6896004037bea8f6f
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2019-06-11 17:02:07 +0200

    update create CA script

--- create_CA.sh
+++ create_CA.sh
@@ -8,12 +8,10 @@ usage ()
 	echo -e " USE: ./create_CA.sh [options]"
 	echo -e "   --help or -h : usage information.\n"
     echo "   --create_CA : Create your own CA to secure communications with Elasticsearch nodes. Requires openssl installed and demoCA directory."
-	echo "   --full_creation : Create your own CA to secure communications with Elasticsearch nodes and demoCA directory. Requires openssl installed."
     echo "   --CA_name : CA name for pem certificate and key."
     echo "   --days : Days of duration of the certificate."
 	echo -e " Examples:"
 	echo -e "  ./create_CA.sh --create_CA --CA_name server.TEST-CA --days 18250"
-    echo -e "  ./create_CA.sh --full_creation --CA_name server.TEST-CA --days 18250"
 }
 
 
@@ -32,9 +30,7 @@ while [ "$1" != "" ]; do
 								DAYS=$1
 								;;
 		--create_CA )			ACTION=create_CA
-								;;
-		--full_creation )		ACTION=full_creation
-								;;                                
+								;;                        
 		-h | --help )			usage
 								exit
 								;;
@@ -59,7 +55,6 @@ then
 	exit
 fi
 
-
 if [[ ! $ACTION ]]
 then
 	usage
@@ -74,54 +69,27 @@ create_own_CA ()
 {
     SIGNED_CA="${CA_NAME}-signed.pem"
     KEY_CA="${CA_NAME}.key"
-    CSR_CA="${CA_NAME}.csr"
     echo "Creation of the self-signed CA certificate."
-    echo "Generate CA provate key"
+    echo "Generate CA private key"
     openssl genrsa -des3 -out ${KEY_CA} 2048
-    echo "Create a certificate signing request. Fill in the certificate with your data"
-    openssl req -verbose -new -key ${KEY_CA} -out ${CSR_CA} -sha256
     echo "Self sign certificate"
-    openssl ca -extensions v3_ca -out ${SIGNED_CA} -keyfile ${KEY_CA} -verbose -selfsign -md sha256 -days ${DAYS} -infiles ${CSR_CA}
+    openssl req -x509 -new -nodes -key  ${KEY_CA} -sha256 -days ${DAYS} -out ${SIGNED_CA}
     echo "Self signed CA created"
     cp ${KEY_CA} elasticsearch/config/
     cp ${SIGNED_CA} elasticsearch/config/
     cp ${SIGNED_CA} kibana/config/
     cp ${SIGNED_CA} logstash/config/
-    rm ${SIGNED_CA} ${KEY_CA} ${CSR_CA}
-    sed -i 's:ARG SECURITY_CA_PEM_LOCATION="config/server.TEST-CA-signed.pem":ARG SECURITY_CA_PEM_LOCATION="config/'$SIGNED_CA'":g' elasticsearch/Dockerfile
-    sed -i 's:ARG SECURITY_CA_KEY_LOCATION="config/server.TEST-CA.key":ARG SECURITY_CA_KEY_LOCATION="config/'$KEY_CA'":g' elasticsearch/Dockerfile
-    sed -i 's:ARG SECURITY_CA_PEM_LOCATION="config/server.TEST-CA-signed.pem":ARG SECURITY_CA_PEM_LOCATION="config/'$SIGNED_CA'":g' kibana/Dockerfile
-    sed -i 's:ARG SECURITY_CA_PEM_LOCATION="config/server.TEST-CA-signed.pem":ARG SECURITY_CA_PEM_LOCATION="config/'$SIGNED_CA'":g' logstash/Dockerfile
-    sed -i 's:ARG SECURITY_CA_PEM_ARG="server.TEST-CA-signed.pem":ARG SECURITY_CA_PEM_ARG="'$SIGNED_CA'":g' logstash/Dockerfile
-    sed -i 's:SECURITY_CA_PEM=server.TEST-CA-signed.pem:SECURITY_CA_PEM='$SIGNED_CA':g' docker-compose.yml
-    sed -i 's:SECURITY_CA_KEY=server.TEST-CA.key:SECURITY_CA_KEY='$KEY_CA':g' docker-compose.yml
+    rm ${SIGNED_CA} ${KEY_CA}
     echo "Finished"
 }
 
-full_own_creation ()
-{
-    echo "Create directory demoCA. Press enter and fill in the first certificate with your data."
-    /usr/lib/ssl/misc/CA.pl -newca
-    echo "demoCA directory created."
-    create_own_CA
-}
-
-
-creation_type ()
-{
-    if [[ $ACTION == "create_CA" ]]; then
-        create_own_CA 
-    elif [[ $ACTION == "full_creation" ]]; then
-        full_own_creation 
-    fi
-}
 #------------------------- Creation functions ------------------------------#
 
 #------------------------- Main workflow ------------------------------#
 
 main ()
 {
-    creation_type
+    create_own_CA
 }
 
 
