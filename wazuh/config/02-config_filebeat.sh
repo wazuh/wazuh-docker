e9e5813cd203787e93b7b5af6a6c8b3d77284832
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2019-09-12 10:06:51 +0200

    wazuh alerts and shards will be managed in wazuh image instead of elastic image

--- wazuh/config/02-config_filebeat.sh
+++ wazuh/config/02-config_filebeat.sh
@@ -11,9 +11,12 @@ if [ "$ELASTICSEARCH_URL" != "" ]; then
   sed -i 's|http://elasticsearch:9200|'$ELASTICSEARCH_URL'|g' /etc/filebeat/filebeat.yml
 fi
 
-# Install Wazuh Filebeat Module
 
+# Modify wazuh-alerts shards and replicas
+sed -i 's:"index.number_of_shards"\: "3":"index.number_of_shards"\: "'$WAZUH_ALERTS_SHARDS'":g' /etc/filebeat/wazuh-template.json
+sed -i 's:"index.number_of_replicas"\: "0":"index.number_of_replicas"\: "'$WAZUH_ALERTS_REPLICAS'":g' /etc/filebeat/wazuh-template.json
+
+# Install Wazuh Filebeat Module
 curl -s "https://packages.wazuh.com/3.x/filebeat/${WAZUH_FILEBEAT_MODULE}" | tar -xvz -C /usr/share/filebeat/module
 mkdir -p /usr/share/filebeat/module/wazuh
 chmod 755 -R /usr/share/filebeat/module/wazuh
-
