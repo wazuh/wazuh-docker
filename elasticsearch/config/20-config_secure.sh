fbe9776b33ac863b84b94c49e338effa7410b051
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2019-09-18 13:18:33 +0200

    test without verification

--- elasticsearch/config/20-config_secure.sh
+++ elasticsearch/config/20-config_secure.sh
@@ -90,14 +90,14 @@ instances:
 # Required to set the passwords and TLS options
 xpack.security.enabled: true
 xpack.security.transport.ssl.enabled: true
-xpack.security.transport.ssl.verification_mode: certificate
+xpack.security.transport.ssl.verification_mode: none
 xpack.security.transport.ssl.key: /usr/share/elasticsearch/config/$ELASTIC_HOSTNAME/$ELASTIC_HOSTNAME.key
 xpack.security.transport.ssl.certificate: /usr/share/elasticsearch/config/elasticsearch.cert.pem
 xpack.security.transport.ssl.certificate_authorities: [\"/usr/share/elasticsearch/config/$SECURITY_CA_TRUST\"]
 
 # HTTP layer
 xpack.security.http.ssl.enabled: true
-xpack.security.http.ssl.verification_mode: certificate
+xpack.security.http.ssl.verification_mode: none
 xpack.security.http.ssl.key: /usr/share/elasticsearch/config/$ELASTIC_HOSTNAME/$ELASTIC_HOSTNAME.key
 xpack.security.http.ssl.certificate: /usr/share/elasticsearch/config/elasticsearch.cert.pem
 xpack.security.http.ssl.certificate_authorities: [\"/usr/share/elasticsearch/config/$SECURITY_CA_TRUST\"]
