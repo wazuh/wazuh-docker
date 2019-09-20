12b5e2bd02d4245635d55011fc40d12a5a9cade6
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2019-09-20 16:25:09 +0200

    fix welcome screen

--- kibana/config/welcome_wazuh.sh
+++ kibana/config/welcome_wazuh.sh
@@ -9,12 +9,13 @@ then
     kibana_path="/usr/share/kibana"
     # Set Wazuh app as the default landing page
     echo "Set Wazuh app as the default landing page"
-    echo "server.defaultRoute: /app/wazuh" >> /usr/share/kibana/config/kibana.yml
+    echo "server.defaultRoute: /app/wazuh" >> $kibana_path/config/kibana.yml
 
     # Redirect Kibana welcome screen to Discover
     echo "Redirect Kibana welcome screen to Discover"
     # sed -i "s:'/app/kibana#/home':'/app/wazuh':g" $kibana_path/src/ui/public/chrome/directives/global_nav/global_nav.html
     # sed -i "s:'/app/kibana#/home':'/app/wazuh':g" $kibana_path/src/ui/public/chrome/directives/header_global_nav/header_global_nav.js
+    sed -i "s:'/app/kibana#/home':'/app/wazuh':g" $kibana_path/src/core/public/chrome/chrome_service.js
 
     # Hide management undesired links
     echo "Hide management undesired links"
