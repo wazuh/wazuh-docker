912785b5c8e60cfb4131221c5378ac40c6cf9941
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2019-09-26 10:35:36 +0200

    minor change

--- wazuh/config/20-ossec-configuration.sh
+++ wazuh/config/20-ossec-configuration.sh
@@ -5,8 +5,7 @@
 # Change Wazuh manager configuration. 
 ##############################################################################
 
-
-# # Example 
+# # Example: 
 # # Change remote protocol from udp to tcp
 # PROTOCOL="tcp"
 # sed -i -e '/<remote>/,/<\/remote>/ s|<protocol>udp</protocol>|<protocol>'$PROTOCOL'</protocol>|g' /var/ossec/etc/ossec.conf
