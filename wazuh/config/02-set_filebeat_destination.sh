cb7839cb9e486ffa8ac91f4988cf33285ea41b58
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2019-10-30 09:10:14 +0100

    improve set filebeat output

--- wazuh/config/02-set_filebeat_destination.sh
+++ wazuh/config/02-set_filebeat_destination.sh
@@ -9,12 +9,17 @@ if [[ $FILEBEAT_DESTINATION == "elasticsearch" ]]; then
 
     echo "FILEBEAT - Set destination to Elasticsearch"
     cp filebeat_to_elasticsearch.yml /etc/filebeat/filebeat.yml
+    if [[ $FILEBEAT_OUTPUT != "" ]]; then
+        sed -i "s/elasticsearch:9200/$FILEBEAT_OUTPUT:9200/" /etc/filebeat/filebeat.yml
+    fi
 
 elif [[ $FILEBEAT_DESTINATION == "logstash" ]]; then
 
     echo "FILEBEAT - Set destination to Logstash"
     cp filebeat_to_logstash.yml /etc/filebeat/filebeat.yml
-    sed -i "s/logstash:5000/$FILEBEAT_OUTPUT:5000/" /etc/filebeat/filebeat.yml
+    if [[ $FILEBEAT_OUTPUT != "" ]]; then
+        sed -i "s/logstash:5000/$FILEBEAT_OUTPUT:5000/" /etc/filebeat/filebeat.yml
+    fi
 
 else
     echo "FILEBEAT - Error choosing destination. Set default filebeat.yml "
