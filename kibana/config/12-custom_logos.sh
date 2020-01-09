9e531f1a351ab1a8e9757ef959e9b5490dff03af
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2020-01-09 12:44:08 +0100

    add custom logos for kibana script

---
+++ kibana/config/12-custom_logos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Wazuh App Copyright (C) 2019 Wazuh Inc. (License GPLv2)
+
+##############################################################################
+# Kibana logos
+##############################################################################
+
+if [[ $CUSTOM_LOGO == "true" ]]; then
+
+
+    echo "CUSTOM LOGO - Change Kibana logos."
+    # TO DO
+
+fi
