7e7f97c4cd95f3bbc48f3f7d6992875fbce77c73
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2020-03-04 12:24:18 +0100

    Add trap for sbin my init  (#310)

--- wazuh/config/entrypoint.sh
+++ wazuh/config/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Wazuh Docker Copyright (C) 2019 Wazuh Inc. (License GPLv2)
 
+# Trap to kill container if it is necessary.
+trap "exit" SIGINT SIGTERM
 # It will run every .sh script located in entrypoint-scripts folder in lexicographical order
 for script in `ls /entrypoint-scripts/*.sh | sort -n`; do
   bash "$script"
