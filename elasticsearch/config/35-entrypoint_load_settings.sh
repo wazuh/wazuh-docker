99d54f1776280e0f1818517e6c4814f97715fc58
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2020-03-25 18:45:58 +0100

    Adapt to 3.11.4_7.4.2 (#314)
    Former-commit-id: 7fe1c6bd1becdf710c9d2e3964fd4768660720b7

--- elasticsearch/config/35-entrypoint_load_settings.sh
+++ elasticsearch/config/35-entrypoint_load_settings.sh
@@ -28,13 +28,9 @@ echo "LOAD SETTINGS - Elasticsearch url: $el_url"
 ##############################################################################
 
 ELASTIC_PASS=""
-WAZH_API_USER=""
-WAZH_API_PASS=""
 
 if [[ "x${SECURITY_CREDENTIALS_FILE}" == "x" ]]; then
   ELASTIC_PASS=${SECURITY_ELASTIC_PASSWORD}
-  WAZH_API_USER=${API_USER}
-  WAZH_API_PASS=${API_PASS}
 else
   input=${SECURITY_CREDENTIALS_FILE}
   while IFS= read -r line
@@ -42,12 +38,6 @@ else
     if [[ $line == *"ELASTIC_PASSWORD"* ]]; then
       arrIN=(${line//:/ })
       ELASTIC_PASS=${arrIN[1]}
-    elif [[ $line == *"WAZUH_API_USER"* ]]; then
-      arrIN=(${line//:/ })
-      WAZH_API_USER=${arrIN[1]}
-    elif [[ $line == *"WAZUH_API_PASSWORD"* ]]; then
-      arrIN=(${line//:/ })
-      WAZH_API_PASS=${arrIN[1]}
     fi
   done < "$input"
  
@@ -154,102 +144,8 @@ fi
 
 
 ##############################################################################
-# Prepare Wazuh API credentials
+# Enable xpack.monitoring.collection
 ##############################################################################
-
-API_PASS_Q=`echo "$WAZH_API_PASS" | tr -d '"'`
-API_USER_Q=`echo "$WAZH_API_USER" | tr -d '"'`
-API_PASSWORD=`echo -n $API_PASS_Q | base64`
-
-echo "LOAD SETTINGS - Setting API credentials into Wazuh APP"
-CONFIG_CODE=$(curl -s -o /dev/null -w "%{http_code}" -XGET $el_url/.wazuh/_doc/1513629884013 ${auth})
-
-if [ "x$CONFIG_CODE" != "x200" ]; then
-
-  curl -s ${auth} -X PUT "$el_url/.wazuh/?pretty" -H 'Content-Type: application/json' -d'
-  {
-    "settings" : {
-          "number_of_shards" : 1,
-          "number_of_replicas" : 0,
-          "auto_expand_replicas": "0-1"
-    }
-  }
-  '
-  curl -s -XPOST $el_url/.wazuh/_doc/1513629884013 ${auth} -H 'Content-Type: application/json' -d'
-  {
-    "api_user": "'"$API_USER_Q"'",
-    "api_password": "'"$API_PASSWORD"'",
-    "url": "'"$wazuh_url"'",
-    "api_port": "55000",
-    "insecure": "true",
-    "component": "API",
-    "cluster_info": {
-      "manager": "wazuh-manager",
-      "cluster": "Disabled",
-      "status": "disabled"
-    },
-    "extensions": {
-      "oscap": true,
-      "audit": true,
-      "pci": true,
-      "aws": true,
-      "virustotal": true,
-      "gdpr": true,
-      "ciscat": true
-    }
-  }
-  ' > /dev/null
-else
-  echo "LOAD SETTINGS - Wazuh APP already configured"
-  echo "LOAD SETTINGS - Check if it is an upgrade from Elasticsearch 6.x to 7.x"
-  wazuh_search_request=`curl -s ${auth} "$el_url/.wazuh/_search?pretty"`
-  full_type=`echo $wazuh_search_request | jq .hits.hits | jq .[] | jq ._type`
-  elasticsearch_request=`curl -s $auth "$el_url"`
-  full_elasticsearch_version=`echo $elasticsearch_request | jq .version.number`
-  type=`echo "$full_type" | tr -d '"'`
-  elasticsearch_version=`echo "$full_elasticsearch_version" | tr -d '"'`
-  elasticsearch_major="${elasticsearch_version:0:1}"
-
-  if [[ $type == "wazuh-configuration" ]] && [[ $elasticsearch_major == "7" ]]; then
-    echo "LOAD SETTINGS - Elasticsearch major = $elasticsearch_major."
-    echo "LOAD SETTINGS - Reindex .wazuh in .wazuh-backup."
-    
-    curl -s ${auth} -XPOST "$el_url/_reindex" -H 'Content-Type: application/json' -d'
-    {
-      "source": {
-        "index": ".wazuh"
-      },
-      "dest": {
-        "index": ".wazuh-backup"
-      }
-    }
-    '
-    echo "LOAD SETTINGS - Remove .wazuh index."
-    curl -s  ${auth} -XDELETE "$el_url/.wazuh"
-
-    echo "LOAD SETTINGS - Reindex .wazuh-backup in .wazuh."
-    curl -s ${auth} -XPOST "$el_url/_reindex" -H 'Content-Type: application/json' -d'
-    {
-      "source": {
-        "index": ".wazuh-backup"
-      },
-      "dest": {
-        "index": ".wazuh"
-      }
-    }
-    '
-    curl -s ${auth} -XPUT "https://elasticsearch:9200/.wazuh-backup/_settings?pretty" -H 'Content-Type: application/json' -d'
-    {
-        "index" : {
-            "number_of_replicas" : 0
-        }
-    }
-    '
-
-  fi
-
-fi
-sleep 5
 
 curl -XPUT "$el_url/_cluster/settings" ${auth} -H 'Content-Type: application/json' -d'
 {
@@ -258,6 +154,7 @@ curl -XPUT "$el_url/_cluster/settings" ${auth} -H 'Content-Type: application/jso
   }
 }
 '
+
 
 ##############################################################################
 # Set cluster delayed timeout when node falls
