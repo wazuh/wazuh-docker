1394f849db1eae37772e4e4d9945412d5d69eea6
Mayte Ariza <mayte.ariza@wazuh.com>
2020-04-22 16:53:01 +0200

    Modified function comments

--- wazuh/config/01-wazuh.sh
+++ wazuh/config/01-wazuh.sh
@@ -35,10 +35,10 @@ exec_cmd_stdout() {
 ##############################################################################
 # Check_update
 # This function considers the following cases:
-# - If /var/ossec/etc/ossec-init.conf does not exist -> There is no data in the EBS. First time deploying Wazuh 
-# - If /var/ossec/etc/VERSION does not exist -> Update (The previous version was prior to 3.11.5) 
-# - If both files exist: different Wazuh version -> Update (The previous version was later to 3.11.5)
-# - If both files exist: the same Wazuh version -> Current version matches the upcoming one
+# - If /var/ossec/etc/ossec-init.conf does not exist -> Action Nothing. There is no data in the EBS. First time deploying Wazuh
+# - If /var/ossec/etc/VERSION does not exist -> Action: Update. The previous version was prior to 3.11.5.
+# - If both files exist: different Wazuh version -> Action: Update. The previous version is older than the current one.
+# - If both files exist: the same Wazuh version -> Acton: Nothing. Same Wazuh version.
 ##############################################################################
 
 check_update() {
@@ -46,11 +46,11 @@ check_update() {
   then
     if [ -e /var/ossec/etc/VERSION ]
     then
-      current_version=$(cat /var/ossec/etc/VERSION | grep -i version | cut -d'"' -f2)
+      previous_version=$(cat /var/ossec/etc/VERSION | grep -i version | cut -d'"' -f2)
+      echo "Previous version: $previous_version"
+      current_version=$(cat ${WAZUH_INSTALL_PATH}/data_tmp/permanent/var/ossec/etc/ossec-init.conf | grep -i version | cut -d'"' -f2)
       echo "Current version: $current_version"
-      upcoming_version=$(cat ${WAZUH_INSTALL_PATH}/data_tmp/permanent/var/ossec/etc/ossec-init.conf | grep -i version | cut -d'"' -f2)
-      echo "Upcoming version: $upcoming_version"
-      if [ $current_version == $upcoming_version ]
+      if [ $previous_version == $current_version ]
       then
         echo "Same Wazuh version in the EBS and image"
         return 0
