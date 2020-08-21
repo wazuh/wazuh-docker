0c6114698615da12dd6abb7d4245d5be728700a7
Manuel Gutierrez <1380243+xr09@users.noreply.github.com>
2020-08-21 15:40:35 +0200

    Customize login styles

--- kibana-opendistro/config/welcome_wazuh.sh
+++ kibana-opendistro/config/welcome_wazuh.sh
@@ -3,22 +3,12 @@
 
 if [[ $CHANGE_WELCOME == "true" ]]
 then
-
-    rm -rf ./optimize/bundles
-
-    kibana_path="/usr/share/kibana"
-    # Set Wazuh app as the default landing page
     echo "Set Wazuh app as the default landing page"
     echo "server.defaultRoute: /app/wazuh" >> /usr/share/kibana/config/kibana.yml
 
-    # Redirect Kibana welcome screen to Discover
-    echo "Redirect Kibana welcome screen to Discover"
-    sed -i "s:'/app/kibana#/home':'/app/wazuh':g" $kibana_path/src/ui/public/chrome/directives/global_nav/global_nav.html
-    sed -i "s:'/app/kibana#/home':'/app/wazuh':g" $kibana_path/src/ui/public/chrome/directives/header_global_nav/header_global_nav.js
-
-    # Redirect Kibana welcome screen to Discover
-    echo "Hide undesired links"
-    sed -i 's#visible: true#visible: false#g' $kibana_path/node_modules/x-pack/plugins/rollup/public/crud_app/index.js
-    sed -i 's#visible: true#visible: false#g' $kibana_path/node_modules/x-pack/plugins/license_management/public/management_section.js
+    echo "Set custom welcome styles"
+    cp -f /tmp/custom_welcome/template.js.hbs /usr/share/kibana/src/legacy/ui/ui_render/bootstrap/template.js.hbs
+    cp -f /tmp/custom_welcome/security-login.style.css /usr/share/kibana/optimize/bundles/security-login.style.css
+    cp -f /tmp/custom_welcome/*svg /usr/share/kibana/optimize/bundles/
 fi
 
