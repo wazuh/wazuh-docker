7ee06250ac9c8cecf6e9cf04adb1ac3685e17c49
AlfonsoRBJ <alfonso.ruiz-bravo@wazuh.com>
2020-11-09 15:51:28 +0100

    test

--- wazuh/config/01-wazuh.sh
+++ wazuh/config/01-wazuh.sh
@@ -225,6 +225,8 @@ EOF
     # create or customize API user
     if /var/ossec/framework/python/bin/python3  /var/ossec/framework/scripts/create_user.py; then
       # remove json if exit code is 0
+      echo "CHECK CREDENTIALS"
+      cat /var/ossec/api/configuration/admin.json
       rm /var/ossec/api/configuration/admin.json
     else
       cat /var/ossec/api/configuration/admin.json
