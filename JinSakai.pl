7658e13a8efba09fe6a37a1e48c0a1692422c1ce
nu11secur1ty <venvaropt@gmail.com>
2020-11-15 15:03:23 +0200

    Update JinSakai.pl

--- JinSakai.pl
+++ JinSakai.pl
@@ -10,9 +10,13 @@ use Term::ANSIColor;
 chomp(my $IP_ = `hostname -I | cut -d' ' -f1`);
 
 # Checking and install packages if this is necessary
+# Here you can modify for your Linux distribution!
+# This Program is for Ubuntu 20.04.x
 my $pack_ = `apt-get install docker docker-compose git vim gcc -y`;
 
 # Managing performance
+my $setup_max_map = `sed -i 's/vm.max_map_count=262144/vm.max_map_count=262144/g' /etc/sysctl.conf`;
+# Fast way
 my $korendil = `sysctl -w vm.max_map_count=262144`;
 
 # Installing
@@ -22,5 +26,5 @@ print color('bold blue');
   print "Now you can catch the cyber bandits!\n";
   print color('reset');
     print color('red');
-  print "WARNING: For outside monitoring of your SIEM, please use your real or local IP address, for example: https://$IP_/app/wazuh\n";
+  print "WARNING: For outside monitoring of your SIEM, please use your real IP address! Your local IP and link is: https://$IP_/app/wazuh\n";
     print color('reset');
