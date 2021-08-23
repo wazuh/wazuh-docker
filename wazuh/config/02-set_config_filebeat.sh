75fc45ca25e4c58cee032bb36beb9ce67399422b
Julio Reyes <julio.reyes@wazuh.com>
2021-08-23 10:28:30 +0200

    Updated to use 4.x
    Former-commit-id: 6f82840cec37c3ff0e57c55a61fcca1354d05554

--- wazuh/config/02-set_config_filebeat.sh
+++ wazuh/config/02-set_config_filebeat.sh
@@ -5,7 +5,10 @@ set -e
 
 ##############################################################################
 # Set Filebeat config.  
+
 ##############################################################################
+
+WAZUH_FILEBEAT_MODULE=wazuh-filebeat-0.1.tar.gz
 
 echo "FILEBEAT - Copy Filebeat config file"
 cp filebeat.yml /etc/filebeat/filebeat.yml
@@ -17,6 +20,6 @@ chmod go-w /etc/filebeat/filebeat.yml
 echo "FILEBEAT - Get Filebeat Wazuh module"
 
 >&2 echo "FILEBEAT - Install Wazuh Filebeat Module."
-curl -s "https://packages.wazuh.com/3.x/filebeat/${WAZUH_FILEBEAT_MODULE}" | tar -xvz -C /usr/share/filebeat/module
+curl -s "https://packages.wazuh.com/4.x/filebeat/${WAZUH_FILEBEAT_MODULE}" | tar -xvz -C /usr/share/filebeat/module
 mkdir -p /usr/share/filebeat/module/wazuh
 chmod 755 -R /usr/share/filebeat/module/wazuh
