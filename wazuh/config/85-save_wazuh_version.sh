dd797edf51143c0ca21b4829bdc72733543aa917
AlfonsoRBJ <sitoruizbravo@hotmail.com>
2021-09-16 20:20:22 +0200

    Adapt cloud to v4.2.1 (#488)

--- wazuh/config/85-save_wazuh_version.sh
+++ wazuh/config/85-save_wazuh_version.sh
@@ -3,4 +3,4 @@
 
 # Copy /var/ossec/etc/ossec-init.conf contents in /var/ossec/etc/VERSION to be able to check the previous Wazuh version in pod.
 echo "Adding Wazuh version to /var/ossec/etc/VERSION"
-cat /var/ossec/etc/ossec-init.conf > /var/ossec/etc/VERSION
+/var/ossec/bin/wazuh-control info > /var/ossec/etc/VERSION
