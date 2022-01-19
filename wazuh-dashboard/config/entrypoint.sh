c6e018691778dda6d015f0985c4d1f53c48817b3
vcerenu <victor.erenu@wazuh.com>
2022-01-19 17:20:46 -0300

    Add wazuh-dashboard Dockerfile

---
+++ wazuh-dashboard/config/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Wazuh Docker Copyright (C) 2021 Wazuh Inc. (License GPLv2)
+
+##############################################################################
+# Start Wazuh dashboard
+##############################################################################
+
+service wazuh-dashboard start
+
+service wazuh-dashboard enable
