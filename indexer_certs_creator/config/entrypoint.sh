8327d5bed29fb7641855c883217501e9322dd7ba
vcerenu <victor.erenu@wazuh.com>
2022-01-24 17:35:44 -0300

    Add Wazuh indexer cluster configuration

---
+++ indexer_certs_creator/config/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Wazuh Docker Copyright (C) 2021 Wazuh Inc. (License GPLv2)
+
+##############################################################################
+# Start Wazuh indexer
+##############################################################################
+
+/usr/share/wazuh-indexer/plugins/opensearch-security/tools/wazuh-cert-tool.sh
