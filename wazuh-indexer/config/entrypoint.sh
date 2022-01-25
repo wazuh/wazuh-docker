ede856a7cf20f80b53f3f15580e8a3f3babcc651
vcerenu <victor.erenu@wazuh.com>
2022-01-25 12:29:04 -0300

    Add Wazuh indexer cluster configuration

--- wazuh-indexer/config/entrypoint.sh
+++ wazuh-indexer/config/entrypoint.sh
@@ -5,10 +5,19 @@
 # Start Wazuh indexer
 ##############################################################################
 
+rm -rf /var/lib/wazuh-indexer/*
+
 service wazuh-indexer start
 
 sleep 20
 
-export JAVA_HOME=/usr/share/wazuh-indexer/jdk/ &&  bash /usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh -cd /usr/share/wazuh-indexer/plugins/opensearch-security/securityconfig/ -nhnv -cacert /etc/wazuh-indexer/certs/root-ca.pem -cert /etc/wazuh-indexer/certs/admin.pem -key /etc/wazuh-indexer/certs/admin-key.pem -p 9800 -icl
+echo "inicio ver hostname"
+echo $HOSTNAME
+echo "fin ver hostname"
+export OPENSEARCH_PATH_CONF=/etc/wazuh-indexer 
+export JAVA_HOME=/usr/share/wazuh-indexer/jdk
+/usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh -cd /usr/share/wazuh-indexer/plugins/opensearch-security/securityconfig -icl -p 9800 -cd /usr/share/wazuh-indexer/plugins/opensearch-security/securityconfig -nhnv -cacert /etc/wazuh-indexer/certs/root-ca.pem -cert /etc/wazuh-indexer/certs/admin.pem -key /etc/wazuh-indexer/certs/admin-key.pem
+
+#export JAVA_HOME=/usr/share/wazuh-indexer/jdk/ &&  bash /usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh -cd /usr/share/wazuh-indexer/plugins/opensearch-security/securityconfig/ -nhnv -cacert /etc/wazuh-indexer/certs/root-ca.pem -cert /etc/wazuh-indexer/certs/admin.pem -key /etc/wazuh-indexer/certs/admin-key.pem -p 9800 -icl
 
 tail -f /var/log/wazuh-indexer/wazuh-cluster.log
