39d01d51b1a6dcc2006f46c2e81c3f47efdc887c
gx1 <giper@users.noreply.github.com>
2022-01-28 19:04:36 +0100

    Templating production_cluster folder

---
+++ production_cluster.tpl/kibana_ssl/generate-self-signed-cert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+if [ -s key.pem ]
+then
+    echo "Certificate already exists"
+    exit
+else
+    openssl req -x509 -batch -nodes -days 365 -newkey rsa:2048 -keyout key.pem -out cert.pem
+    chown -R 1000:1000 *.pem
+fi
