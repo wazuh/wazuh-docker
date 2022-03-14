71b7a87705c73e6dda4444d8bb115e63b6a7989a
Gonzalo Acuña <teddytpc1@gmail.com>
2022-03-14 14:53:48 -0300

    Cert tool URL updates

--- indexer_certs_creator/config/entrypoint.sh
+++ indexer_certs_creator/config/entrypoint.sh
@@ -2,6 +2,21 @@
 # Wazuh Docker Copyright (C) 2021 Wazuh Inc. (License GPLv2)
 
 ##############################################################################
+# Downloading Cert Gen Tool
+##############################################################################
+
+FILE=wazuh-cert-tool.sh
+
+#Download from packages.wazuh.com with first parameter
+curl -o $FILE https://packages.wazuh.com/4.x/wazuh-cert-tool.sh
+var=`grep NoSuchKey $FILE`
+
+#If the content of the file contains NoSuchKey, download from packages-dev.wazuh.com
+if [ ! -z "$var" ]; then
+  curl -o $FILE  https://packages-dev.wazuh.com/4.3/wazuh-certs-tool.sh
+fi
+
+##############################################################################
 # Creating Cluster certificates
 ##############################################################################
 
