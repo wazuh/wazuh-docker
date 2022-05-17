020031c81d71266550df7a476da6621a7f101242
vcerenu <victor.erenu@wazuh.com>
2022-05-17 15:49:38 -0300

    Modify Dashboard user parameter

--- build-docker-images/wazuh-dashboard/config/entrypoint.sh
+++ build-docker-images/wazuh-dashboard/config/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # Wazuh Docker Copyright (C) 2021 Wazuh Inc. (License GPLv2)
 
+INSTALL_DIR=/usr/share/wazuh-dashboard
+DASHBOARD_USERNAME="${DASHBOARD_USERNAME:-kibanaserver}"
+DASHBOARD_PASSWORD="${DASHBOARD_PASSWORD:-kibanaserver}"
+
+# Create and configure Wazuh dashboard keystore
+
+$INSTALL_DIR/bin/opensearch-dashboards-keystore create --allow-root && \
+echo $DASHBOARD_USERNAME | $INSTALL_DIR/bin/opensearch-dashboards-keystore add opensearch.username --stdin --allow-root && \
+echo $DASHBOARD_PASSWORD | $INSTALL_DIR/bin/opensearch-dashboards-keystore add opensearch.password --stdin --allow-root
+
 ##############################################################################
 # Start Wazuh dashboard
 ##############################################################################
