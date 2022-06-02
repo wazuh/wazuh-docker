0f2fd8417342bef99b57152555bb50bcc84b6edc
vcerenu <victor.erenu@wazuh.com>
2022-06-02 15:47:59 -0300

    Test CI

--- build-images.sh
+++ build-images.sh
@@ -2,7 +2,7 @@ WAZUH_IMAGE_VERSION=4.3.1
 WAZUH_VERSION=$(echo $WAZUH_IMAGE_VERSION | sed -e 's/\.//g')
 WAZUH_ACTUAL_VERSION=$(curl --silent https://api.github.com/repos/wazuh/wazuh/releases/latest | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/' | cut -c 2- | sed -e 's/\.//g')
 
-## If wazuh manager exists in apt dev repository, change variables, if not exit 1
+## If wazuh manager exists in apt dev repository, change variables, if not, exit 1
 if [ "$WAZUH_VERSION" -le "$WAZUH_ACTUAL_VERSION" ]; then
   IMAGE_VERSION=${WAZUH_IMAGE_VERSION}
 else
