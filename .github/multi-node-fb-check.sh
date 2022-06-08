a34e0af547fc6e29da8cc952fb7f111e30eeb845
vcerenu <victor.erenu@wazuh.com>
2022-06-08 18:14:12 -0300

    Test CI

--- .github/multi-node-fb-check.sh
+++ .github/multi-node-fb-check.sh
@@ -1,5 +1,5 @@
 fbout1=$(docker exec multi-node_wazuh.master_1 sh -c 'filebeat test output')
-fbstatus1=$(echo "${fbout1}" | grep OK | wc -l)
+fbstatus1=$(echo "${fbout1}" | grep -c OK)
 if [[ fbstatus1 -eq 7 ]]; then
  echo "No errors in master filebeat"
 else
@@ -8,7 +8,7 @@ else
  exit 1
 fi
 fbout2=$(docker exec multi-node_wazuh.worker_1 sh -c 'filebeat test output')
-fbstatus2=$(echo "${fbout2}" | grep OK | wc -l)
+fbstatus2=$(echo "${fbout2}" | grep -c OK)
 if [[ fbstatus2 -eq 7 ]]; then
  echo "No errors in worker filebeat"
 else
--- .github/single-node-fb-check.sh
+++ .github/single-node-fb-check.sh
@@ -1,5 +1,5 @@
 fbout=$(docker exec single-node_wazuh.manager_1 sh -c 'filebeat test output')
-fbstatus=$(echo "${fbout}" | grep OK | wc -l)
+fbstatus=$(echo "${fbout}" | grep -c OK)
 if [[ fbstatus -eq 7 ]]; then
   echo "No errors in filebeat"
 else
