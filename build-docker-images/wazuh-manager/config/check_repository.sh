c3375e0141e479805ef9271d1c2d750349c82212
vcerenu <victor.erenu@wazuh.com>
2022-06-21 11:46:25 -0300

    modify variable

--- build-docker-images/wazuh-manager/config/check_repository.sh
+++ build-docker-images/wazuh-manager/config/check_repository.sh
@@ -1,8 +1,8 @@
 ## Variables
 WAZUH_IMAGE_VERSION=$(echo $WAZUH_VERSION | sed -e 's/\.//g')
-WAZUH_ACTUAL_VERSION=$(curl --silent https://api.github.com/repos/wazuh/wazuh/releases/latest | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/' | cut -c 2- | sed -e 's/\.//g')
+WAZUH_CURRENT_VERSION=$(curl --silent https://api.github.com/repos/wazuh/wazuh/releases/latest | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/' | cut -c 2- | sed -e 's/\.//g')
 ## If wazuh manager exists in apt dev repository, change variables, if not exit 1
-if [ "$WAZUH_IMAGE_VERSION" -le "$WAZUH_ACTUAL_VERSION" ]; then
+if [ "$WAZUH_IMAGE_VERSION" -le "$WAZUH_CURRENT_VERSION" ]; then
   APT_KEY=https://packages.wazuh.com/key/GPG-KEY-WAZUH
   REPOSITORY="deb https://packages.wazuh.com/4.x/apt/ stable main"
 else
