292941430ec63858ba812d8716e17a42701f694b
TerrifiedBug <35064668+TerrifiedBug@users.noreply.github.com>
2024-02-22 12:52:04 +0000

    Delete .github directory

--- .github/single-node-filebeat-check.sh
+++
@@ -1,9 +0,0 @@
-filebeatout=$(docker exec single-node_wazuh.manager_1 sh -c 'filebeat test output')
-filebeatstatus=$(echo "${filebeatout}" | grep -c OK)
-if [[ filebeatstatus -eq 7 ]]; then
-  echo "No errors in filebeat"
-else
-  echo "Errors in filebeat"
-  echo "${filebeatout}"
-  exit 1
-fi
--- .github/single-node-log-check.sh
+++
@@ -1,8 +0,0 @@
-log=$(docker exec single-node_wazuh.manager_1 sh -c 'cat /var/ossec/logs/ossec.log' | grep -P "ERR|WARN|CRIT")
-if [[ -z "$log" ]]; then
-  echo "No errors in ossec.log"
-else
-  echo "Errors in ossec.log:"
-  echo "${log}"
-  exit 1
-fi
