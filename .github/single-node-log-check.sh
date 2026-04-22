119bd3afd710c109eab39b74cb2369d262980bc7
wazuhci <22834044+wazuhci@users.noreply.github.com>
2026-04-22 19:20:18 +0000

    feat: revert test-revert-option references

--- .github/single-node-log-check.sh
+++
@@ -1,8 +0,0 @@
-log=$(docker exec single-node_wazuh.manager_1 sh -c 'cat /var/wazuh-manager/logs/wazuh-manager.log' | grep -P "ERR|WARN|CRIT")
-if [[ -z "$log" ]]; then
-  echo "No errors in wazuh-manager.log"
-else
-  echo "Errors in wazuh-manager.log:"
-  echo "${log}"
-  exit 1
-fi
